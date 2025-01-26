0d38e8dcd48167e81f799d7086e44c442c00078b
Sam Nejati <sam4nzd@gmail.com>
2025-01-26 15:06:44 +0100

    Assembler e deserialize

--- crates/dronegowski_hosts/src/enums.rs
+++ crates/dronegowski_hosts/src/enums.rs
@@ -1,6 +1,7 @@
 use wg_2024::network::NodeId;
 use wg_2024::packet::Packet;
 use crossbeam_channel::{Sender};
+use serde::{Deserialize, Serialize};
 
 pub enum ClientEvent{
     PacketSent(Packet), // Avvisa il SC che è stato inviato un pacchetto
@@ -17,6 +18,32 @@ pub enum ClientType {
     WebBrowsers,
     ChatClients,
 }
+
+
+// Definizione di strutture dati e tipi personalizzati
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct CustomStruct {
+    id: u32,
+    name: String,
+    data: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+enum CustomEnum {
+    Variant1(String),
+    Variant2 { id: u32, value: f64 },
+}
+
+// Enum per rappresentare diversi tipi di messaggi
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+enum TestMessage {
+    Text(String),
+    Number(u32),
+    Vector(Vec<u8>),
+    Struct(CustomStruct),
+    Enum(CustomEnum),
+}
+
 
 pub enum ServerType {
     ContentServer,
